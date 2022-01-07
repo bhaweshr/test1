3e89fdef1c598f82e236111bb48e9a793a25a201
Bhawesh <Bhawesh@DESKTOP-TAACCJI.mshome.net>
2022-01-07 12:35:57 +0530

    adding new Method calculate

--- test1/src/com/bhawesh/git/test1/Test1.java
+++ test1/src/com/bhawesh/git/test1/Test1.java
@@ -5,6 +5,12 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello git2");
+System.out.println(Test1.calculate(3,5));
+	}
+	
+	public static int calculate(int a,int b) {
+		
+		return a+b;
 	}
 
 }
